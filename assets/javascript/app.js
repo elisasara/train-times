5a7527452f597ca3b3d38758040f4e33115b7181
Elisa Urda <urda.elisa@gmail.com>
2018-03-16 21:40:54 -0400

    finished basic js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,20 +15,18 @@
 
 $(document).ready(function (){
 
-
+// when the submit button is clicked
 
     $("#submitButton").on("click", function (){
         event.preventDefault();
 
+        // get the values of the inputs
         var name = $("#name").val().trim();
-        console.log(name);
         var destination = $("#destination").val().trim();
-        console.log(destination);
         var firstTime = $("#firstTrain").val().trim();
-        console.log(firstTime);
         var frequency = $("#frequency").val().trim();
-        console.log(frequency);
 
+        // push those values into firebase
         database.ref().push({
             trainName : name,
             destination : destination,
@@ -37,19 +35,43 @@ $(document).ready(function (){
         });
     });
 
+    // when a child is added in firebase
+    database.ref().on("child_added", function(childSnapshot) {
 
-    database.ref().on("child_added", function(snapshot) {
-        var trainName = snapshot.val().trainName;
-        var destination = snapshot.val().destination;
-        var firstTrain = snapshot.val().firstTrain;
-        var frequency = snapshot.val().frequency;
+        // store the value that is in firebase
+        var trainName = childSnapshot.val().trainName;
+        var destination = childSnapshot.val().destination;
+        var firstTrain = childSnapshot.val().firstTrain;
+        console.log(firstTrain);
+        var frequency = childSnapshot.val().frequency;
+        
+        // conver the input to a time format
+        var timeForMath = moment(firstTrain, "HH:mm:ss").format();
+        console.log(timeForMath);
 
-        $("#timeSchedule").append("<tr><td>" + trainName +"</td><td>" + destination + "</td><td>" +  firstTrain + "</td><td>" + frequency + "</tr>");
+        // format the time in am/pm manner
+        var formattedTime = moment(firstTrain, "HH:mm:ss").format("hh:mm a");
+        console.log(formattedTime);
+
+        // find the difference in minutes between the current time and the time of the first train
+        var difference = moment().diff(timeForMath, "minutes");
+        console.log(difference);
+
+        // get the remainder of minutes left when the difference is divided by the frequency
+        var remainder = difference % frequency;
+        console.log(remainder);
+
+        // add the remainder to the current time to get the time of the next train
+        var nextTrain = moment().add(remainder, "minutes").format("hh:mm a");
+        console.log(nextTrain);
+
+        // display all of the fields in a new row in the table
+        $("#timeSchedule").append("<tr><td>" + trainName +"</td><td>" + destination + "</td><td>" +  frequency + "</td><td>" + nextTrain + "</td><td>" + remainder + "</td></tr>");
 
     });
         
 
 
-    })
+    });
 
 
